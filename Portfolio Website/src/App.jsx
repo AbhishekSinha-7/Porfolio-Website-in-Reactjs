fa86987ac7481fccfb87c13253f578622e3b691e
AbhishekSinha-7 <curiousindian16@gmail.com>
2024-10-20 16:57:56 +0530

    fixed iimports of components in app.jsx

--- Portfolio Website/src/App.jsx
+++ Portfolio Website/src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-
 import './App.css'
 
-import Header from './Components/Header'
-import Home from './Components/Home'
-import About from './Components/About'
-import Contact from './Components/Contact'
+import Header from './components/Header'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
